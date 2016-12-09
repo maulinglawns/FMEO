57ec893c858bd982756ea7ae89077b18e5e0d95f
Magnus Wallin <magnus@localhost.localdomain>
2016-12-09 10:26:10 +0100

    Added protection against multiple failed login attempts

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ delPrev($uploadDir);
 
 $user = "foo";
 $pass = "bar";
+$limit = 2;
 
 $loginForm = <<<EOF
 <form method="POST" action="index.php">
@@ -27,13 +28,30 @@ if (! isset($_SESSION['loggedin']))  {
     $_SESSION['loggedin'] = "false";
 }
 
+// Initialize the failed login var
+if (! isset($_SESSION['failed'])) {
+    $_SESSION['failed'] = 0;
+}
+
+// Redirect after more than $limit attempts at logging in
+if ($_SESSION['failed'] > 2) {
+    header('Location: http://php.net');
+    /* Replace the above with proper error page */
+}   
+
+//debug
+var_dump($_SESSION['failed']);
+
 if ($_SESSION['loggedin'] != "true") {
     if (isset($_POST['submit'])) {
         if ($_POST['user'] == $user && $_POST['pass'] == $pass) {
             $_SESSION['loggedin'] = "true";
+            // Reset the failed login counter
+            $_SESSION['failed'] = 0;
             echo "<p>Du är inloggad.</p>";
             echo "<p>Klicka <a href=\"fileHandle.php\">här</a> för att gå vidare.</p>";
         } else {
+            $_SESSION['failed']++;
             echo "<h1>Fel inloggningsuppgifter!</h1>";
             echo "<p><a href=\"index.php\">Gå tillbaka</a></p>";
         }
